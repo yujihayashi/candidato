6ca78fd354b2efe36d5f89741521f3e8a40261fe
Yuji Hayashi <arthuryuji@hotmail.com>
2014-08-21 18:17:06 -0300

    Area de downloads

--- wp-content/themes/brunomineiro/content-image.php
+++ wp-content/themes/brunomineiro/content-image.php
@@ -9,49 +9,48 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php twentyfourteen_post_thumbnail(); ?>
 
 	<header class="entry-header">
-		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && twentyfourteen_categorized_blog() ) : ?>
-		<div class="entry-meta">
-			<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
-		</div><!-- .entry-meta -->
 		<?php
-			endif;
 
-			if ( is_single() ) :
-				the_title( '<h1 class="entry-title text-primary">', '</h1>' );
-			else :
-				the_title( '<h1 class="entry-title text-primary"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
-			endif;
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title text-primary">', '</h1>' );
+		else :
+			the_title( '<h1 class="entry-title text-primary"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+		endif;
 		?>
 
-		<div class="entry-meta">
-			<span class="post-format">
-				<a class="entry-format" href="<?php echo esc_url( get_post_format_link( 'image' ) ); ?>"><?php echo get_post_format_string( 'image' ); ?></a>
-			</span>
-
-			<?php twentyfourteen_posted_on(); ?>
-
-			<?php if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) : ?>
-			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyfourteen' ), __( '1 Comment', 'twentyfourteen' ), __( '% Comments', 'twentyfourteen' ) ); ?></span>
-			<?php endif; ?>
-
-			<?php edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' ); ?>
-		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
-
 	<div class="entry-content">
-		<?php
-			the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ) );
-			wp_link_pages( array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
-				'after'       => '</div>',
-				'link_before' => '<span>',
-				'link_after'  => '</span>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+		<div class="media">
+			<span class="pull-left"><?php the_post_thumbnail('large'); ?></span>
+			<div class="media-body">
 
-	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
+
+				<div class="descricao">
+					<?php the_content(); ?>
+				</div> <!-- .descricao -->
+				<?php 
+				if ( has_post_thumbnail() ) {
+					$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
+					$filetype = wp_check_filetype($large_image_url[0]);
+					echo '<a href="' . $large_image_url[0] . '" class="" title="' . the_title_attribute( 'echo=0' ) . '" download="'.the_title_attribute( 'echo=0' ).'.'.$filetype['ext'].'">';
+					echo 'Download da imagem';
+					echo '</a>';        
+				}
+				?>
+				<?php edit_post_link( __( 'Edit', 'twentyfourteen' ), ' / <span class="edit-link">', '</span>' ); ?>
+				<hr>
+				<div id="fb-root"></div>
+				<script>(function(d, s, id) {
+					var js, fjs = d.getElementsByTagName(s)[0];
+					if (d.getElementById(id)) return;
+					js = d.createElement(s); js.id = id;
+					js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&appId=607367415995227&version=v2.0";
+					fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));</script>
+				<div class="fb-like" data-href="<? the_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+			</div> <!-- .media-body -->
+		</div> <!-- .media -->
+	</div><!-- .entry-content -->
 </article><!-- #post-## -->
