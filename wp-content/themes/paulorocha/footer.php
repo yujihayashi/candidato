a73ad241affb47f4d9cde6da93373090d2d2a243
Yuji Hayashi <arthuryuji@hotmail.com>
2014-09-05 09:22:55 -0300

    Analytics

--- wp-content/themes/paulorocha/footer.php
+++ wp-content/themes/paulorocha/footer.php
@@ -79,6 +79,16 @@
 </footer><!-- #colophon -->
 </div><!-- #page -->
 <?php wp_footer(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-10928573-36', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <!-- <script src="http://localhost:35729/livereload.js"></script> -->
 </body>
 </html>
